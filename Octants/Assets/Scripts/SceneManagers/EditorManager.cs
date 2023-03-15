d50c0bc6fa626cb5682a2fbba206f14ba2592748
ASiracusa <31045746+ASiracusa@users.noreply.github.com>
2023-03-15 16:27:23 -0400

    Adding and Deleting Ground Tiles

--- Octants/Assets/Scripts/SceneManagers/EditorManager.cs
+++ Octants/Assets/Scripts/SceneManagers/EditorManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,7 @@ using UnityEngine;
 public class EditorManager : MonoBehaviour
 {
     private Vector3Int levelDims;
-    private Vector3Int[] groundSpaces;
+    private GameObject[,,] groundGos;
     private Vector3 zeroCorner;
 
     private Vector3Int playerSpace;
@@ -13,18 +14,27 @@ public class EditorManager : MonoBehaviour
     private int playerDir;
 
     private int sigilCount;
-    private Vector3Int[] sigilSpaces;
-    private int[] sigilColors;
+    private List<Vector3Int> sigilSpaces;
+    private List<int> sigilColors;
 
     private int reflectorCount;
-    private Vector3Int[] reflectorSpaces;
-    private Vector2Int[] reflectorColors;
-    private bool[][] reflectorDims;
+    private List<Vector3Int> reflectorSpaces;
+    private List<Vector2Int> reflectorColors;
+    private List<bool[]> reflectorDims;
 
     private float cameraCurrentRot = 0;
     private float cameraTargetRot = 0;
+
+    private GameObject gsPrefab;
+    private Material evenMat;
+    private Material oddMat;
+    private Transform groundParent;
     
     private GameObject cursorCube;
+    private Vector3Int cursorPos;
+    private bool selecting;
+    private bool leftSelect;
+    private Vector3Int selectPos;
 
     void Start()
     {
@@ -33,27 +43,24 @@ public class EditorManager : MonoBehaviour
         // Creates a generic floor out of ground tiles
         levelDims = new Vector3Int(15, 9, 15);
         zeroCorner = -0.5f * (Vector3)(levelDims - new Vector3Int(1, 1, 1));
-        List<Vector3Int> grounds = new List<Vector3Int>();
-        for (int x = 0; x < levelDims.x; x++) {
-            for (int z = 0; z < levelDims.z; z++) {
-                grounds.Add(new Vector3Int(x, 0, z));
-            }
-        }
-        groundSpaces = grounds.ToArray();
+        groundGos = new GameObject[levelDims.x, levelDims.y, levelDims.z];
 
         // Spawns the floor
-        GameObject gsPrefab = (GameObject)Resources.Load("Prefabs/LevelElements/GroundTile");
-        Material evenMat = (Material)Resources.Load("Materials/EvenGround");
-        Material oddMat = (Material)Resources.Load("Materials/OddGround");
-        Transform groundParent = GameObject.Find("LevelElements/GroundElements").transform;
-        foreach (Vector3Int gsPos in groundSpaces) {
-            GameObject gs = Instantiate(gsPrefab, groundParent);
-            gs.name = "GroundElement";
-            gs.transform.position = zeroCorner + gsPos;
-            if ((gsPos.x + gsPos.y + gsPos.z) % 2 == 1) {
-                gs.GetComponent<MeshRenderer>().material = oddMat;
-            } else {
-                gs.GetComponent<MeshRenderer>().material = evenMat;
+        gsPrefab = (GameObject)Resources.Load("Prefabs/LevelElements/GroundTile");
+        evenMat = (Material)Resources.Load("Materials/EvenGround");
+        oddMat = (Material)Resources.Load("Materials/OddGround");
+        groundParent = GameObject.Find("LevelElements/GroundElements").transform;
+        for (int x = 0; x < levelDims.x; x++) {
+            for (int z = 0; z < levelDims.z; z++) {
+                GameObject gs = Instantiate(gsPrefab, groundParent);
+                groundGos[x,0,z] = gs;
+                gs.name = "GroundElement";
+                gs.transform.position = zeroCorner + new Vector3(x, 0, z);
+                if ((x + z) % 2 == 1) {
+                    gs.GetComponent<MeshRenderer>().material = oddMat;
+                } else {
+                    gs.GetComponent<MeshRenderer>().material = evenMat;
+                }
             }
         }
     }
@@ -77,7 +84,7 @@ public class EditorManager : MonoBehaviour
         if (Physics.Raycast(cameraRay, out cursorHit, 100)) {
             cursorCube.SetActive(true);
             if (cursorHit.transform.gameObject.layer == 0) {
-                Vector3Int cursorPos = Vector3Int.FloorToInt(cursorHit.transform.position - zeroCorner);
+                cursorPos = Vector3Int.FloorToInt(cursorHit.transform.position - zeroCorner);
                 if (!Input.GetKey(KeyCode.LeftShift)) {
                     cursorPos += DirectionFromPosition(cursorHit.point, cursorHit.transform.position);
                 }
@@ -85,7 +92,43 @@ public class EditorManager : MonoBehaviour
             }
         } else {
             cursorCube.SetActive(false);
+            cursorPos = -1 * Vector3Int.one;
         }
+    }
+
+    void Update() {
+
+        // Adding and removing ground tiles
+        if ((Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1)) && cursorCube.activeSelf) {
+            selecting = true;
+            leftSelect = Input.GetMouseButtonDown(0);
+            selectPos = cursorPos;
+        }
+        if ((Input.GetMouseButtonUp(0) || Input.GetMouseButtonUp(1)) && cursorCube.activeSelf && selecting) {
+            selecting = false;
+            Tuple<Vector3Int, Vector3Int> corners = MinMaxVectors(selectPos, cursorPos);
+            for (int x = corners.Item1.x; x <= corners.Item2.x; x++) {
+                for (int y = corners.Item1.y; y <= corners.Item2.y; y++) {
+                    for (int z = corners.Item1.z; z <= corners.Item2.z; z++) {
+                        if (leftSelect && groundGos[x,y,z] == null) {
+                            GameObject gs = Instantiate(gsPrefab, groundParent);
+                            groundGos[x,y,z] = gs;
+                            gs.name = "GroundElement";
+                            gs.transform.position = zeroCorner + new Vector3(x, y, z);
+                            if ((x + y + z) % 2 == 1) {
+                                gs.GetComponent<MeshRenderer>().material = oddMat;
+                            } else {
+                                gs.GetComponent<MeshRenderer>().material = evenMat;
+                            }
+                        } else if (!leftSelect && groundGos[x,y,z] != null) {
+                            Destroy(groundGos[x,y,z]);
+                            groundGos[x,y,z] = null;
+                        }
+                    }
+                }
+            }
+        }
+
     }
 
     private Vector3Int DirectionFromPosition (Vector3 point, Vector3 home) {
@@ -113,5 +156,10 @@ public class EditorManager : MonoBehaviour
             }
         }
         return Vector3Int.zero;
+    }
+
+    private Tuple<Vector3Int, Vector3Int> MinMaxVectors (Vector3Int a, Vector3Int b) {
+        return Tuple.Create(new Vector3Int(Mathf.Min(a.x, b.x), Mathf.Min(a.y, b.y), Mathf.Min(a.z, b.z)),
+                            new Vector3Int(Mathf.Max(a.x, b.x), Mathf.Max(a.y, b.y), Mathf.Max(a.z, b.z))); 
     }
 }
